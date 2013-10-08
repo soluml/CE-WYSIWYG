3f8d4ccee6b014fda9c1831ddc9b2186797f02f3
Benjamin Solum <benjamin@soluml.com>
2013-10-08 14:26:28 -0500

    Update

--- js/jquery.cewysiwyg.js
+++ js/jquery.cewysiwyg.js
@@ -5,21 +5,21 @@
         var settings = $.extend({
             $sel : $('textarea'),
             btns : {
-                    html        : true,
-                    element     : true,
-                    bold        : true,
-                    italic      : true,
-                    underline   : true,
-                    strike      : true,
-                    super       : true,
-                    sub         : true,
-                    quote       : true,
-                    numberlist  : true,
-                    orderlist   : true,
-                    alignleft   : true,
-                    alignright  : true,
-                    aligncenter : true,
-                    alignjustify: true
+                    html        : {'use':true, 'char':'h', 'title':'HTML View'},
+                    element     : {'use':true, 'char':'e', 'title':'Insert Element'},
+                    bold        : {'use':true, 'char':'b', 'title':'Bold'},
+                    italic      : {'use':true, 'char':'i', 'title':'Italic'},
+                    underline   : {'use':true, 'char':'u', 'title':'Underline'},
+                    strike      : {'use':true, 'char':'s', 'title':'Strikethrough'},
+                    super       : {'use':true, 'char':' ', 'title':'Superscript'},
+                    sub         : {'use':true, 'char':' ', 'title':'Subscript'},
+                    quote       : {'use':true, 'char':'q', 'title':'Quote'},
+                    numberlist  : {'use':true, 'char':' ', 'title':'Numbered List'},
+                    orderlist   : {'use':true, 'char':' ', 'title':'Ordered List'},
+                    alignleft   : {'use':true, 'char':'l', 'title':'Align Left'},
+                    alignright  : {'use':true, 'char':'r', 'title':'Align Right'},
+                    aligncenter : {'use':true, 'char':'c', 'title':'Align Center'},
+                    alignjustify: {'use':true, 'char':'j', 'title':' Align Justified'}
             }
         }, options);
 
@@ -34,8 +34,8 @@
                 html = '<ul>';
 
             for(k in settings.btns)
-                if(settings.btns[k] === true)
-                    html += '<li class="jquery-wysiwyg-'+k+'"></li>';
+                if(settings.btns[k].use === true)
+                    html += '<li class="jquery-wysiwyg-'+k+'" title="'+settings.btns[k].title+'">'+settings.btns[k].char+'</li>';
 
             html += '</ul><div class="jquery-wysiwyg-contenteditable" contenteditable="true">'+$t.val()+'</div>';
             $s = $('<div class="jquery-wysiwyg"></div>').insertAfter($t).html(html);
